c89ce4c1b8b548e6a8be1bad92731528e6b2e549
Mouhamed DIOP <mouhamed061100@outlook.fr>
2025-05-17 12:10:14 +0000

    Update choisir_role.php

--- choisir_role.php
+++ choisir_role.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) { 
     header("Location: authentification.php");
     exit();
 }
