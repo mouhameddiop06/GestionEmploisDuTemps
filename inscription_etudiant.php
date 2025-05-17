d31cfd5b2a0ff2a66b51de1773cec68108cd1dd7
Mouhamed DIOP <mouhamed061100@outlook.fr>
2025-05-17 12:10:59 +0000

    Update inscription_etudiant.php

--- inscription_etudiant.php
+++ inscription_etudiant.php
@@ -1,7 +1,7 @@
 <?php
 include 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
     $nom = $_POST['nom']; 
     $prenom = $_POST['prenom'];
     $email = $_POST['email_inscription'];
