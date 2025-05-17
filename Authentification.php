a38ad868869c27c3cbe666eedbf8138ca731cfdf
Mouhamed DIOP <mouhamed061100@outlook.fr>
2025-05-17 12:09:41 +0000

    Update Authentification.php

--- Authentification.php
+++ Authentification.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head> 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Connexion et Inscription</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
